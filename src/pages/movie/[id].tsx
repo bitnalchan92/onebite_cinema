71c7e106895f85066a879ae67258fcdf7bdf2b68
bitnalchan92 <bitnalchan92@gmail.com>
2025-04-02 13:32:47 +0900

    [day5] 4 구현, 완료

--- src/pages/movie/[id].tsx
+++ src/pages/movie/[id].tsx
@@ -1,8 +1,42 @@
 import {useRouter} from "next/router";
+import {MovieData} from "@/types";
+import movies from '@/mock/dummy.json';
+import style from './[id].module.css';
 
 export default function Page() {
   const router = useRouter();
   const {id} = router.query;
 
-  return <h1>{id} 영화 상세페이지</h1>;
+  const foundMovie = movies.find((movie) => movie.id.toString() === id);
+  if ( !foundMovie ) {
+    return (
+      <div>
+        <div style={{marginTop: '20px'}}>검색하신 영화를 찾을 수 없습니다.</div>
+      </div>
+    );
+  }
+
+  const {
+    title,
+    subTitle,
+    description,
+    releaseDate,
+    company,
+    genres,
+    runtime,
+    posterImgUrl
+  }: MovieData = foundMovie;
+
+  return (
+    <div className={style.container}>
+      <div style={{backgroundImage: `url('${posterImgUrl}')`}} className={style.cover_img_container}>
+        <img src={posterImgUrl} alt={title}/>
+      </div>
+      <div className={style.title}>{title}</div>
+      <div>{releaseDate} / {genres.join(', ')} / {runtime}분</div>
+      <div>{company}</div>
+      <div className={style.subTitle}>{subTitle}</div>
+      <div className={style.description}>{description}</div>
+    </div>
+  );
 }
